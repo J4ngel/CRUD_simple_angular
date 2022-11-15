c0b664c006bfb48a95702874c193a6c78ff64987
J4ngel <andres_angel@live.com.ar>
2022-11-15 02:03:46 -0500

    Se agregaron los paquetes a usarse en este proyecto, se desarrolló la página principal y se crearon algunas clases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './models/empleado';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CRUD_simple';
+  arreglo_de_empleados: Empleado[] = [
+    {id:1, name:"Daniela", country:"Colombia"},
+    {id:2, name:"Nicolas", country:"Colombia"},
+    {id:3, name:"Sergio", country:"Colombia"}
+  ];
+
+  empleado_seleccionado: Empleado = new Empleado();
+
+  addOrEdit(){
+    if(this.empleado_seleccionado.id === 0){
+      this.empleado_seleccionado.id = this.arreglo_de_empleados.length+1;
+      this.arreglo_de_empleados.push(this.empleado_seleccionado);
+    }
+    
+    this.empleado_seleccionado = new Empleado();
+  }
+
+  openForEdit(empleado: Empleado){
+    this.empleado_seleccionado = empleado;
+  }
+
+  delete(){
+    if(confirm('¿Está seguro de querer eliminar el usuario seleccionado?')){
+      this.arreglo_de_empleados = this.arreglo_de_empleados.filter(x => x!=this.empleado_seleccionado);
+      this.empleado_seleccionado = new Empleado();
+    }
+  }
 }
